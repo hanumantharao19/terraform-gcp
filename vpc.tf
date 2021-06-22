f48a78f33b078ebe66ac539d524bda599168d8ee
hanumantharao19 <63094959+hanumantharao19@users.noreply.github.com>
2021-06-22 09:16:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,11 @@
-resource "google_compute_network" "vpc-mhr" {
-
-
-  name                    = "vpc-mhr-dev"
-  auto_create_subnetworks = false
+resource "google_compute_subnetwork" "subenet-mhr-hr" {
+  name          = var.subnetname
+  ip_cidr_range = var.cidr
+  region        = var.region
+  network       = google_compute_network.vpc-mhr-hr.id
 }
-resource "google_compute_subnetwork" "subnet-mhr" {
 
-
-  name          = "subnet-mhr-dev"
-  ip_cidr_range = "10.1.9.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.vpc-mhr.id
+resource "google_compute_network" "vpc-mhr-hr" {
+  name                    = var.vpcname
+  auto_create_subnetworks = false
 }
